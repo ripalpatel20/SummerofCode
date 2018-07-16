32900847924c27ff459756ea75d2976ed7626d47
Ripal Patel <patelripal.job@gmail.com>
2018-07-16 14:43:42 -0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#ponies
+
+print(1+2)
+print(2*3)
+print(5-8)
+print(9/2)
+
+print(5 * (12-8) + -15)
+print(98 + (59872 / (13*8)) * -51)
+
+# modulus: gives you the remainder after division
+print(10 % 2)
+
+# power and bitshift
+# credit: alteredco "mathletes"
+print(2**8)
+print(1<<9)
+
+#How many hours in year?
+print(365*24)
+
+#How many minutes in decade?
+print(10*365*24*60)
+
+#How many seconds are you old?
+print(30*365*24*60*60)
